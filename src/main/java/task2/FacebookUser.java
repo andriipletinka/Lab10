1752b8231d7838b3ae530916071cda947197e2a2
andriipletinka <andrii.pletinka@ucu.edu.ua>
2022-11-27 20:47:01 +0200

    tests

--- src/main/java/task2/FacebookUser.java
+++ src/main/java/task2/FacebookUser.java
@@ -5,21 +5,6 @@ import lombok.Getter;
 
 import java.time.LocalDate;
 
-enum Country {
-    Ukraine("Ukraine"), USA("USA"), Poland("Poland");
-
-    public final String string;
-
-    private Country(String string) {
-        this.string = string;
-    }
-
-    @Override
-    public String toString() {
-        return string;
-    }
-}
-
 @Getter
 @AllArgsConstructor
 public class FacebookUser {
